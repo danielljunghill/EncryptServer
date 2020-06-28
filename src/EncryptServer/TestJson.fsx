c85f07e4d8a07ad747655f1c44b2fe325fc16223
daniel.ljunghill <daniel.ljunghill@hotmail.com>
2020-06-28 22:06:41 +0200

    add newtonsoft ref to test

--- src/EncryptServer/TestJson.fsx
+++ src/EncryptServer/TestJson.fsx
@@ -1,6 +1,6 @@
 ﻿open System
-#r @"C:\Users\dalj\.nuget\packages\system.text.json\4.7.2\lib\netcoreapp3.0\System.Text.Json.dll"
-#r @"./bin/debug/netcoreapp3.1/EncryptServer.dll"
+#r @"C:\Users\dalj\.nuget\packages\newtonsoft.json\12.0.3\lib\net45\Newtonsoft.Json.dll"
+#load "Library.fs"
 
 
 let v = EncryptServer.Test.kb
