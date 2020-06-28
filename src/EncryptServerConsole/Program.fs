22f5ccd101866a85dc84acea7ff6d97b6b76d514
daniel.ljunghill <daniel.ljunghill@hotmail.com>
2020-06-28 21:46:08 +0200

    add project

---
+++ src/EncryptServerConsole/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open EncryptServer
+[<EntryPoint>]
+let main argv =
+
+    printfn "Hello World from F#!"
+    let v = Test.kb
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
+
