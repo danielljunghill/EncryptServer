132595bbc071aa6bf13d8a2d49ef28acefb163ca
daniel.ljunghill <daniel.ljunghill@hotmail.com>
2020-06-30 18:57:00 +0200

    create keys from password

--- src/EncryptServer/TestEncrypt.fsx
+++ src/EncryptServer/TestEncrypt.fsx
@@ -45,14 +45,13 @@ let aes1 = Aes.Create()
 let key = aes1.Key
 let iv = aes1.IV
 
-let createAes key iv =
+let createAes() =
     let aes = Aes.Create()
     aes.KeySize <- 256
     aes.BlockSize <- 128
     aes.FeedbackSize <- 128
     aes.Padding <- PaddingMode.Zeros  
-    aes.IV <- iv
-    aes.Key <- key
+
     aes
 
 let decryptEncrypt ict bts =
@@ -64,12 +63,12 @@ let decryptEncrypt ict bts =
 
 
 let encrypt key iv (bts:byte[]) =
-    let aes = createAes key iv  
+    let aes = createAes ()  
     let ict = aes.CreateEncryptor(key,iv) 
     decryptEncrypt ict bts
 
 let decrypt key iv (encBts:byte[]) =
-    let aes = createAes key iv 
+    let aes = createAes()
  
     let ict = aes.CreateDecryptor(key,iv)
     decryptEncrypt ict encBts
@@ -83,6 +82,34 @@ let encBts = encryptor bts
 encBts.Length
 let decBts = decryptor encBts 
 
+
+module Password =
+    let createSalt length =
+        let bts = Array.zeroCreate<byte> length
+        let rand = new RNGCryptoServiceProvider()
+        rand.GetBytes(bts)
+        bts
+
+    let getPasswordDeriveBytes salt (pwd: string) =
+        let pwdBts = System.Text.Encoding.Unicode.GetBytes(pwd) 
+        let tdes = new TripleDESCryptoServiceProvider()
+        let pdb = new PasswordDeriveBytes(pwd,salt,"SHA512",10)
+        pdb
+    let getAesFromPassword salt pwd  =
+        let aes = createAes()
+        let pdb = getPasswordDeriveBytes salt pwd
+        aes.Key <- pdb.CryptDeriveKey("AES","SHA1",256,aes.IV)
+
+    let getKeyAndIVFromPassword (algorithm: SymmetricAlgorithm) (salt: byte[]) pwd =
+         let rfc2898DeriveBytes = new Rfc2898DeriveBytes(pwd, salt);
+         let key = rfc2898DeriveBytes.GetBytes(algorithm.KeySize / 8);
+         let iv =  rfc2898DeriveBytes.GetBytes(algorithm.BlockSize / 8); 
+         key,iv  
+let salt = Password.createSalt 128
+let alg = (SymmetricAlgorithm.Create("AES"))
+alg.KeySize <-256
+
+Password.getKeyAndIVFromPassword alg  salt "Kakadua12"
 
 
 
